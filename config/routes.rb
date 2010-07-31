2b43da912cca858962339640c282885670f66342
Evan Machnic <emachnic@gmail.com>
2010-07-31 00:55:56 -0400

    Added root_path and began layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Aitenvirotech::Application.routes.draw do |map|
+  get "welcome/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Aitenvirotech::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
