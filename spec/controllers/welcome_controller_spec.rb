d6d82d51953da0106a92924bb1349cd5fc04f911
Evan Machnic <emachnic@gmail.com>
2010-08-11 00:18:40 -0400

    Changed welcome page layout

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -4,10 +4,17 @@ describe WelcomeController do
   render_views
 
   describe "GET 'index'" do
+    
     it "should be successful" do
       get 'index'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'index'
+      response.should have_selector("title", :content => "AIT Environmental Technology | Welcome")
+    end
+    
   end
 
 end
